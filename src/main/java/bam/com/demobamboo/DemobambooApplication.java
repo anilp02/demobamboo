8027d26298c08849aa25421f63c9d8f75e515345
anilp02 <anilkumarp31@gmail.com>
2018-06-14 03:43:14 -0400

    init

---
+++ src/main/java/bam/com/demobamboo/DemobambooApplication.java
@@ -0,0 +1,12 @@
+package bam.com.demobamboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobambooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobambooApplication.class, args);
+	}
+}
